0546d7cfb023c02927a91e075a3b3a371bf87c58
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-07-30 20:06:46 +0100

    Allow lambda to pass multiple arguments

--- utils/Lambda.py
+++ utils/Lambda.py
@@ -1,4 +1,5 @@
 import numpy as np
+#from numba import njit
 
 class Lambda(object):
     def __init__(self, function):
@@ -7,7 +8,7 @@ class Lambda(object):
     def begin(self):
         self.lambda_function = eval('lambda '+self.function)
 
-    def __call__(self, event):
+    def __call__(self, *event):
         if not hasattr(self, "lambda_function"):
             self.begin()
-        return self.lambda_function(event)
+        return self.lambda_function(*event)
