a03bb9951156321b4732d3af5ea0199f5ffc6f10
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-08-03 09:34:43 +0100

    Catch errors in Geometry functions

--- utils/Geometry.py
+++ utils/Geometry.py
@@ -33,7 +33,7 @@ def LorTHPMToXYZE(t, h, p, m):
 @njit
 def LorXYZEToTHPM(x, y, z, e):
     t = np.sqrt(x**2+y**2)
-    h = np.arctanh(z/np.sqrt(t**2+z**2))
+    h = np.sign(z)*np.infty if t==0. else np.arctanh(z/np.sqrt(t**2+z**2))
     p = BoundPhi(np.arctan2(y, x))
     m2 = e**2 - t**2 - z**2
     m = np.sign(m2) * np.sqrt(abs(m2))
