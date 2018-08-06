a0cd845520728eebd0c3b2e2ca6d65135535b532
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-08-06 23:02:19 +0100

    Add logger for atuproot.py module

--- atuproot/AtUproot.py
+++ atuproot/AtUproot.py
@@ -1,6 +1,12 @@
 import alphatwirl
 import logging
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 from .EventBuilderConfigMaker import EventBuilderConfigMaker
 from .EventBuilder import EventBuilder
 from .build_parallel import build_parallel
