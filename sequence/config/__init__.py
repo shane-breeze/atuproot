dd0d1b4c466b9945bde3647122885d9a3bf1fe28
Ben Krikler <bek07@ic.ac.uk>
2018-08-14 04:30:37 +0200

    Add missing nullcollector

--- sequence/config/__init__.py
+++ sequence/config/__init__.py
@@ -1,6 +1,7 @@
 from sequence.sequence import sequence
 from sequence.collectors import reader_collectors
 from sequence.Modules import ScribblerWrapper
+from alphatwirl.loop import NullCollector
 
 
 def build_sequence(sequence_cfg_path):
