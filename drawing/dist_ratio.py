bc8637aa152131da74dbd161f694ba299c0d0d3b
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-10 11:58:07 +0100

    Improve dist_ratio and add functionality

--- drawing/dist_ratio.py
+++ drawing/dist_ratio.py
@@ -4,12 +4,15 @@ from misc.cms_tdr_style import cms_tdr_style
 #from misc.draw_line import draw_line
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import spline
 
 def taper_and_drop(hist):
     hist["counts"][-2] += hist["counts"][-1]
     hist["yields"][-2] += hist["yields"][-1]
     hist["variance"][-2] += hist["variance"][-1]
 
+    if isinstance(hist["bins"], list):
+        hist["bins"] = hist["bins"][0]
     hist["bins"] = hist["bins"][1:-1]
     hist["counts"] = hist["counts"][1:-1]
     hist["yields"] = hist["yields"][1:-1]
@@ -35,11 +38,14 @@ def dist_ratio((hist_data, hists_mc, filepath, cfg)):
     hist_mc_sum = {
         "name": hists_mc[0]["name"],
         "sample": hists_mc[0]["sample"],
-        "bins": hists_mc[0]["bins"],
+        "bins": bins,
         "counts": sum(h["counts"] for h in hists_mc),
         "yields": sum(h["yields"] for h in hists_mc),
         "variance": sum(h["variance"] for h in hists_mc),
     }
+    if "function" in hists_mc[0]:
+        hist_mc_sum["function"] = hists_mc[0]["function"]
+    mc_sum = hist_mc_sum["yields"].sum()
     hists_mc = sorted(hists_mc, key=lambda x: x["yields"].sum())
 
     # merge small
@@ -50,7 +56,7 @@ def dist_ratio((hist_data, hists_mc, filepath, cfg)):
     hist_mc_minor = {
         "name": hists_mc[0]["name"],
         "sample": "Minor",
-        "bins": hists_mc[0]["bins"],
+        "bins": bins,
         "counts": sum(h["counts"] for h in hists_mc_lower_oneperc),
         "yields": sum(h["yields"] for h in hists_mc_lower_oneperc),
         "variance": sum(h["variance"] for h in hists_mc_lower_oneperc),
@@ -65,16 +71,14 @@ def dist_ratio((hist_data, hists_mc, filepath, cfg)):
 
     axtop.hist(
         [h["yields"] for h in hists_mc],
-        bins = hists_mc[0]["bins"],
-        log = True,
+        bins = bins,
+        log = cfg.log,
         stacked = True,
         color = [cfg.sample_colours[h["sample"]]
                  if h["sample"] in cfg.sample_colours
                  else "blue"
                  for h in hists_mc],
-        label = [cfg.sample_names[h["sample"]]
-                 if h["sample"] in cfg.sample_names
-                 else h["sample"]
+        label = [h["sample"]
                  for h in hists_mc],
     )
 
@@ -90,11 +94,24 @@ def dist_ratio((hist_data, hists_mc, filepath, cfg)):
             color = cfg.sample_colours[hist_data["sample"]] \
                     if hist_data["sample"] in cfg.sample_colours \
                     else "black",
-            label = cfg.sample_names[hist_data["sample"]] \
-                     if hist_data["sample"] in cfg.sample_names \
-                     else hist_data["sample"],
+            label = hist_data["sample"],
         )
         axtop.set_xlim(bins[0], bins[-1])
+
+    if hist_data is not None and "function" in hist_data:
+        xs, ys = hist_data["function"]
+        ys = mc_sum*ys / sum(ys)
+        xnew = np.linspace(xs.min(), xs.max(), xs.shape[0]*4)
+        ynew = spline(xs, ys, xnew)
+        axtop.plot(xnew, ynew, color="k", ls='--', label="Data fit")
+
+    if "function" in hist_mc_sum:
+        xs, ys = hist_mc_sum["function"]
+        ys = mc_sum*ys / sum(ys)
+        xnew = np.linspace(xs.min(), xs.max(), xs.shape[0]*4)
+        ynew = spline(xs, ys, xnew)
+        axtop.plot(xnew, ynew, color="r", ls='--', label="MC fit")
+
     ymin = max(axtop.get_ylim()[0], 0.5)
     axtop.set_ylim(ymin, None)
 
@@ -111,23 +128,23 @@ def dist_ratio((hist_data, hists_mc, filepath, cfg)):
                fontsize='large')
 
     handles, labels = axtop.get_legend_handles_labels()
-    mc_sum = hist_mc_sum["yields"].sum()
-    labels = ["{} {:.2f}".format(
-                  cfg.sample_names[h["sample"]] \
-                   if h["sample"] in cfg.sample_names \
-                   else h["sample"],
-                  h["yields"].sum()/mc_sum,
-              )
-              for h in hists_mc]
+    handles = handles[::-1]
+    labels = labels[::-1]
+
+    fractions = {
+        h["sample"]: h["yields"].sum()/mc_sum
+        for h in hists_mc
+    }
     if hist_data is not None:
-        labels += ["{} {:.2f}".format(
-                       cfg.sample_names[hist_data["sample"]] \
-                        if hist_data["sample"] in cfg.sample_names \
-                        else hist_data["sample"],
-                       hist_data["yields"].sum()/mc_sum,
-                   )]
-    axtop.legend(handles[::-1], labels[::-1],
-                 labelspacing = 0.1)
+        fractions[hist_data["sample"]] = hist_data["yields"].sum()/mc_sum
+
+    labels = ["{} {:.2f}".format(cfg.sample_names[label], fractions[label])
+              if label in cfg.sample_names else label for label in labels]
+
+    if not hasattr(cfg, "text"):
+        axtop.legend(handles, labels, labelspacing=0.1)
+    else:
+        axtop.legend(handles, labels, title=cfg.text, labelspacing=0.1)
 
     if hist_data is not None:
         axbot.errorbar(
