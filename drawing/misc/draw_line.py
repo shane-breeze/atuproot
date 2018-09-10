5363df2661e12352c1598732165ac2ff15c698a0
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-10 16:26:18 +0100

    Remove misc - old ROOT crap

--- drawing/misc/draw_line.py
+++
@@ -1,9 +0,0 @@
-import ROOT
-
-def draw_line(*pos, **kwargs):
-    line = ROOT.TLine()
-    line.SetLineStyle(7)
-    if "ndc" in kwargs and kwargs["ndc"]:
-        line.DrawLineNDC(*pos)
-    else:
-        line.DrawLine(*pos)
