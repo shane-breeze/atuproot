080f170b1a2d2a7410ceb92fef0a85a898bd07e9
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-12 10:55:42 +0100

    Allow lambda to take kwargs

--- utils/Lambda.py
+++ utils/Lambda.py
@@ -8,10 +8,10 @@ class Lambda(object):
     def begin(self):
         self.lambda_function = eval('lambda '+self.function)
 
-    def __call__(self, *event):
+    def __call__(self, *event, **kwargs):
         if not hasattr(self, "lambda_function"):
             self.begin()
         try:
-            return self.lambda_function(*event)
+            return self.lambda_function(*event, **kwargs)
         except Exception as e:
             raise AttributeError(e, self.function)
