f21520b163f4ea3fb20b8b91fe555b22587a6042
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-19 16:00:15 +0100

    Remove sequence from PYTHONPATH

--- setup_conda.sh
+++ setup_conda.sh
@@ -2,5 +2,5 @@
 export PATH="~/miniconda2/bin:$PATH"
 source activate atuproot
 
-export PYTHONPATH=$PYTHONPATH:$PWD:$PWD/atuproot:$PWD/sequence
+export PYTHONPATH=$PYTHONPATH:$PWD:$PWD/atuproot
 export TOPDIR=$PWD
