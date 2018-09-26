e8e6af6d87f1531fcffa76cf136bbdcec953c7d7
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-26 16:40:49 +0100

    Remove atuproot-methods and analysis specifics from this repository

--- sequence/config/__init__.py
+++
@@ -1,8 +0,0 @@
-import imp
-from sequence.Readers import ScribblerWrapper
-
-
-def build_sequence(sequence_cfg_path):
-    seq = imp.load_source('sequence.sequence', sequence_cfg_path)
-    return [(ScribblerWrapper(reader), collector)
-            for (reader, collector) in seq.sequence]
--- setup_conda.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH="~/miniconda2/bin:$PATH"
-source activate atuproot
-
-export PYTHONPATH=$PYTHONPATH:$PWD:$PWD/atuproot
-export TOPDIR=$PWD
