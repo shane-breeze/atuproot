15865ac75bf35059aa818a649a64d64fa2fb2ee8
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-06-15 08:40:57 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as fh:
 
 setuptools.setup(
     name = "atuproot",
-    version = "0.1.14",
+    version = "0.2.0",
     author = "Shane Breeze",
     author_email = "sdb15@ic.ac.uk",
     scripts = [],
